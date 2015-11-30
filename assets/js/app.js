2005818a54944242d599f95455cb0d58346d94d4
Andreas Glasø Skifjeld <andreas.g.skifjeld@gmail.com>
2015-11-30 14:54:15 +0100

    Testing mock backend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,4 @@
-var app = angular.modoule(Suppliers);
+var app = angular.module('suppliers', ['ngMockE2E'])
+  .run(function ($httpBackend) {
+    $httpBackend.whenGET('/api/category').respond(['cat_1', 'cat_2']);
+  });
